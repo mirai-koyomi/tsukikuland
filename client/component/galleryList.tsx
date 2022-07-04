73677477ff3255ac8940d7b5808119d68e706547
mirai-koyomi <crossclose00@gmail.com>
2022-07-04 20:39:21 +0900

    アニメーションの基本処理の実装

--- client/component/galleryList.tsx
+++ client/component/galleryList.tsx
@@ -1,5 +1,5 @@
-import React, { CSSProperties, FC, useMemo, useRef, useState, WheelEventHandler } from "react"
-import {useTransition, animated, useSprings} from "react-spring"
+import React, { FC, useCallback, useEffect, useRef, useState, WheelEventHandler } from "react"
+import {animated, useSprings} from "react-spring"
 
 interface IGalleryListProps {
   list: {
@@ -16,85 +16,17 @@ const GalleryList: FC<IGalleryListProps> = ({list}) => {
   let lastTime = 0
   const interval = 1000
 
-  const beforeItemNum = useMemo(() => Math.floor((list.length - 1) / 2), [])
-  const rotateList = useMemo(() => {
-    const resultList = [...list]
-    if (currentIdx < beforeItemNum) {
-      resultList.unshift( ...resultList.splice(resultList.length - (beforeItemNum - currentIdx), resultList.length) )
-    } else if (beforeItemNum < currentIdx) {
-      resultList.unshift( ...resultList.splice(currentIdx - beforeItemNum, resultList.length) )
+  const scrollListTo = useCallback((idx: number): {opacity: number, transform: string} => {
+    const deps = Math.abs(currentIdx - idx)
+    return {
+      opacity: deps > 3 ? 0 : 1 - .25 * deps,
+      transform: `scale(${deps > 3 ? 0 : 100 - 25 * deps}%)`,
     }
-
-    return resultList
   }, [currentIdx])
-
-  const nextIdx = (): void => {
-    setCurrentIdx(
-      currentIdx + 1 >= list.length ? 0 : currentIdx + 1
-    )
-  }
-
-  const backIdx = (): void => {
-    setCurrentIdx(
-      currentIdx <= 0 ? list.length - 1 : currentIdx - 1
-    )
-  }
-
-  const listItem = (
-    list: {
-      title: string
-      artist: string
-      synopsis: string
-      thumbnail?: string
-    }[]
-  ): JSX.Element[] => {
-    return list.map((item, i) => {
-      if (beforeItemNum > i) {
-        return (
-          <li className={`gallery__item --number-${beforeItemNum - i}`}>{item.title}</li>
-        )
-      } else if (beforeItemNum === i) {
-        return (
-          <li className={`gallery__item`}>{item.title}</li>
-        )
-      } else {
-        return (
-          <li className={`gallery__item --number-${i - beforeItemNum}`}>{item.title}</li>
-        )
-      }
-    })
-  }
-
-  const transitions = useTransition(currentIdx, {
-    from: (current, idx) => {
-      const deps = Math.abs(current - idx)
-      return {
-        opacity: deps > 3 ? 0 : 1 - .25 * deps,
-        transform: `scale(${deps > 3 ? 0 : 1 - .25 * deps}%)`,
-      }
-    },
-    leave: (current, idx) => {
-      const deps = Math.abs(current - idx)
-      return {
-        opacity: deps > 3 ? 0 : 1 - .25 * deps,
-        transform: `scale(${deps > 3 ? 0 : 1 - .25 * deps}%)`,
-      }
-    },
-    enter: {
-      opacity: 1,
-      transform: 'scale(100%)',
-    },
-  })
 
   const [styles, api] = useSprings(
     list.length,
-    idx => {
-      const deps = Math.abs(currentIdx - idx)
-      return {
-        opacity: deps > 3 ? 0 : 1 - .25 * deps,
-        transform: `scale(${deps > 3 ? 0 : 100 - 25 * deps}%)`,
-      }
-    }
+    idx => scrollListTo(idx)
   )
 
   const handleWheel: WheelEventHandler = (e): void => {
@@ -102,22 +34,19 @@ const GalleryList: FC<IGalleryListProps> = ({list}) => {
 
     if (now - lastTime > interval) {
       if (e.deltaY > 0) {
-        setCurrentIdx(idx => (idx + 1) % rotateList.length)
-        api.start(idx => {
-          const deps = Math.abs(currentIdx + 1 - idx)
-          return {
-            opacity: deps > 3 ? 0 : 1 - .25 * deps,
-            transform: `scale(${deps > 3 ? 0 : 100 - 25 * deps}%)`,
-          }
-        })
+        setCurrentIdx(idx => (idx + 1) % list.length)
       } else {
-        setCurrentIdx(idx => (idx - 1) % rotateList.length)
+        setCurrentIdx(idx => (idx - 1) % list.length)
       }
 
 
       lastTime = now
     }
   }
+
+  useEffect(() => {
+    api.start(idx => scrollListTo(idx))
+  }, [currentIdx])
 
   return (
     <div className="gallery" onWheel={handleWheel} ref={scrollArea}>
